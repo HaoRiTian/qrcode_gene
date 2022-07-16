14157f4bbfa1ea553e0fb5aa545085ed3056cd56
HaoRiTian <HaoRiTian@github.com>
2022-07-16 20:51:58 +0800

    perf: 使脚本可在任意路径下执行

--- bin/build_for_win.sh
+++ bin/build_for_win.sh
@@ -1,2 +1,13 @@
+#!/bin/sh
+
+Script_Dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+Work_Dir="$( cd ${Script_Dir}/.. && pwd )"
+cd ${Work_Dir}
+
+Version=$( git describe --abbrev=0 --tags )
+if [ -z ${Version} ];then
+  Version="Unknown"
+fi
+
 go generate
-GOOS=windows GOARCH=amd64 go build -ldflags "-H windowsgui -w -s" -o build/qrcode_gene.exe
+GOOS=windows GOARCH=amd64 go build -ldflags "-H windowsgui -w -s" -o build/qrcode_gene-${Version}.exe
